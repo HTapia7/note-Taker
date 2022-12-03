5a2e57b17c88d5e886ffa6fa4de8800e81265dde
Hernan Tapia <soliditize@MacBook-Pro.local>
2022-12-02 23:51:21 -0800

    added readme

--- server.js
+++ server.js
@@ -34,9 +34,12 @@ app.post('/api/notes' , (req, res) => {
 })
 
 
-// app.delete('/api/notes/:id' , (req, res) => {
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
-// })
+app.delete('/api/notes/:id' , (req, res) => {
+  
+n
+})
 
 
 // routes for index and notes
